394f32e342ed0444347584d95780076b4fbaa898
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-09-30 08:06:28 +0300

    123

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+let page = 1;
+let searchQuery = '';
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    gallery.innerHTML = ''; // Очистка галереї при новому пошуку
+    page = 1;
+    searchQuery = e.target.searchQuery.value;
+    await fetchImages();
+});
+
+loadMoreButton.addEventListener('click', async () => {
+    page++;
+    await fetchImages();
+});
+
+async function fetchImages() {
+    const apiKey = '39751555-c2fbc931ac716611d03f33f4d'; // Замініть на свій API ключ з Pixabay
+    const perPage = 40;
+    const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.hits.length === 0) {
+            if (page === 1) {
+                // Перший запит і результат порожній
+                alert('Sorry, there are no images matching your search query. Please try again.');
+            } else {
+                // Більше зображень немає
+                loadMoreButton.style.display = 'none';
+                alert("We're sorry, but you've reached the end of search results.");
+            }
+        } else {
+            // Виведення зображень в галерею
+            data.hits.forEach((image) => {
+                const card = document.createElement('div');
+                card.classList.add('photo-card');
+                card.innerHTML = `
+                    <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+                        <p class="info-item"><b>Views:</b> ${image.views}</p>
+                        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+                        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+                    </div>
+                `;
+                gallery.appendChild(card);
+            });
+
+            // Показати кнопку "Load more"
+            loadMoreButton.style.display = 'block';
+        }
+    } catch (error) {
+        console.error('Error fetching images:', error);
+    }
+}
